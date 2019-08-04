a515a08260a3098748d298ec374a0a0d99a36498
Dheeshana <harshinilalindra@gmail.com>
2019-08-05 00:37:37 +0530

    Created Doctor Channeling interface Patient Details interface

--- app/src/main/java/com/example/neethraeyehospital/DoctorChanneling.java
+++ app/src/main/java/com/example/neethraeyehospital/DoctorChanneling.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DoctorChanneling extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_doctor_channeling);
+    }
+
+    public void Search(View view){
+        Intent intent = new Intent(this,DoctorDetails.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/neethraeyehospital/DoctorDetails.java
+++ app/src/main/java/com/example/neethraeyehospital/DoctorDetails.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class DoctorDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_doctor_details);
+    }
+
+    public void Book(View view){
+        Intent intent = new Intent(this,PatientDetails.class);
+        startActivity(intent);
     }
 }
